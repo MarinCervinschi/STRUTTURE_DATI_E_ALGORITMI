ffa1700eddab37612827aa71ae3c9d44427d65d7
MarinCervinschi <marincervinschi3@gmail.com>
2023-06-30 20:40:02 +0200

    es 2 esame 23

--- LAB/Esame di Laboratorio 23/Prodotto Costante/prodotto_costante.c
+++ LAB/Esame di Laboratorio 23/Prodotto Costante/prodotto_costante.c
@@ -1,109 +1,71 @@
 #include <stdlib.h>
 #include <stdbool.h>
-#include <stdio.h>
-
-bool try(int n, bool *vccur) {
-    int k = 0;
-    for (int i = 0; i < n * n; ++i) {
-        if (vccur[i]) {
-            ++k;
+bool IsIn(const int* s, int val, int n) {
+    for (int i = 0; i < n*n; i++) {
+        if (s[i] == val) {
+            return true;
         }
     }
-    return k == n;
+    return false;
 }
-
-void stampa(int n, const int *vector) {
-    printf("[");
-    for (int i = 0; i < n * n - 1; ++i) {
-        printf("%d, ", vector[i]);
+int ProductRow(const int* s, int row, int n) {
+    int res = 1;
+    for (int i = row * n; i < row * n + n; i++) {
+        res *= s[i];
     }
-    printf("%d]\n", vector[n * n - 1]);
+    return res;
 }
-
-bool verifica2(int n, const int *s, const int *vector) {
-    for (int i = 0; i < n * n; ++i) {
-        bool found = false;
-        for (int j = 0; j < n * n; ++j) {
-            if (s[i] == vector[j]) {
-                found = true;
-                break;
-            }
+int ProductCol(const int* s, int row, int n) {
+    int res = 1;
+    for (int i = 0; i < n*n; i+=n) {
+        res *= s[i+row];
+    }
+    return res;
+}
+bool IsValid(int* v_curr, int n) {
+    int val = ProductCol(v_curr, 0, n);
+    for (int i = 1; i < n; i++) {
+        if (ProductCol(v_curr, i, n) != val) {
+            return false;
         }
-        if (!found) {
+    }
+    for (int i = 0; i < n; i++) {
+        if (ProductRow(v_curr, i, n) != val) {
             return false;
         }
     }
     return true;
 }
-
-bool verifica1(int n, const int *vector) {
-    int *sum = calloc(n, sizeof(int));
-    for (int i = 0; i < n; ++i) {
-        sum[i] = 1;
-    }
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = i * n; j < (i + 1) * n; ++j) {
-            sum[i] *= vector[j];
-        }
-    }
-
-    for (int i = 0; i < n - 1; ++i) {
-        if (sum[i] != sum[i + 1]) {
-            free(sum);
-            return false;
-        }
-    }
-
-    free(sum);
-    return true;
-}
-
-void put(int n, const int *s, int *vector, bool *vccur, int lvl) {
-    int j = lvl * n;
-    for (int i = 0; i < n * n; ++i) {
-        if (vccur[i]) {
-            vector[j] = s[i];
-            ++j;
-        }
-    }
-}
-
-void RisolviProdottoRec(int n, const int *s, int *vector, bool *vccur, int i, int *count, int *v_best) {
-    if (i == n) {
-        if (try(n, vccur)) {
-            put(n, s, vector, vccur, *count);
-            ++*count;
-        }
-        if (*count == n) {
-            if (verifica2(n, s, vector) && verifica1(n, vector)) {
-                for (int j = 0; j < n * n; ++j) {
-                    v_best[j] = vector[j];
-                }
-                stampa(n, vector);
-                return;
+void RisolviProdottoRec(int n, const int* s, int* v_curr, bool* found, int lvl, bool * is_in) {
+    if (lvl == n * n && *found == false) {
+        if (IsValid(v_curr, n)) {
+            if (!IsIn(s, -200, n)) {
+                *found = true;
             }
-            *count = 0;
         }
         return;
     }
-
-    for (int j = 0; j < n; ++j) {
-        for (int h = 0; h < n; ++h) {
-            vccur[i + j + h] = true;
-            RisolviProdottoRec(n, s, vector, vccur, i + 1, count, v_best);
-            vccur[i + j + h] = false;
+    for (int i = 0; i < n*n && *found == false; i++) {
+        if (is_in[i] == false) {
+            v_curr[lvl] = s[i];
+            is_in[i] = true;
+            RisolviProdottoRec(n, s, v_curr, found, lvl + 1, is_in);
+            is_in[i] = false;
+            if (*found == false) {
+                v_curr[lvl] = -200;
+            }
         }
     }
 }
-
-int* RisolviProdotto(int n, const int *s) {
-    bool *vccur = calloc(n * n, sizeof(bool));
-    int *vector = calloc(n * n, sizeof(int));
-    int *v_best = calloc(n * n, sizeof(int));
-    int count = 0;
-    RisolviProdottoRec(n, s, vector, vccur, 0, &count, v_best);
-    free(vccur);
-    free(vector);
-    return v_best;
+int* RisolviProdotto(int n, const int* s) {
+    int* v_curr = calloc((size_t)n*n, sizeof(int));
+    bool* is_in = calloc((size_t)n * n, sizeof(bool));
+    bool found = false;
+    RisolviProdottoRec(n, s, v_curr, &found, 0, is_in);
+    free(is_in);
+    if (found) {
+        return v_curr;
+    }
+    free(v_curr);
+    return NULL;
 }
