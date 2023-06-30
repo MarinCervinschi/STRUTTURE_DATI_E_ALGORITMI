9128ed1e851d8a220eb81e63b5a5463a78897915
MarinCervinschi <marincervinschi3@gmail.com>
2023-06-30 15:47:42 +0200

    esame 24

--- LAB/Esame di Laboratorio 24/Populating Next/populatingnext.c
+++ LAB/Esame di Laboratorio 24/Populating Next/populatingnext.c
@@ -1,48 +1,6 @@
-#include <stdio.h>
 #include "tree.h"
-#include "elemtype.h"
 #include <stdlib.h>
 #include <stdbool.h>
-//int NumNode(Node *t){
-//    if(!t){
-//        return 1;
-//    }
-//    return NumNode(t->left) + NumNode(t->right);
-//}
-//void PopulatingNextRec(Node *t, Node *tmp, int *i, int num){
-//    if (!t || TreeIsLeaf(t)) {
-//        ++*i;
-//        return;
-//    }
-//
-//    if (TreeIsLeaf(t->left)) {
-//        ++*i;
-//        t->left->next = t;
-//    }
-//
-//    PopulatingNextRec(t->left, tmp, i, num);
-//
-//    if(TreeIsLeaf(t->right)){
-//        ++*i;
-//        t->next = t->right;
-//        if(*i != num){
-//            t->right->next = tmp;
-//        }
-//    }else{
-//        t->next = t->right->left;
-//    }
-//    PopulatingNextRec(t->right, tmp, i ,num );
-//
-//}
-//void PopulatingNext(Node *t){
-//    if (!t) {
-//        return;
-//    }
-//    int num_node = 0;
-//    num_node = NumNode(t) -1 ;
-//    int i = 0;
-//    PopulatingNextRec(t, t ,&i, num_node);
-//}
 Node* LeftMost(Node* t) {
     while (!TreeIsEmpty(TreeLeft(t))) {
         t = TreeLeft(t);
