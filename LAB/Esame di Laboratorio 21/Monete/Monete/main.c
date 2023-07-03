f0a97bc0ae6356e16b1a8852eddde22a997afabd
MarinCervinschi <marincervinschi3@gmail.com>
2023-07-03 20:27:28 +0200

    exam 21

--- LAB/Esame di Laboratorio 21/Monete/Monete/main.c
+++ LAB/Esame di Laboratorio 21/Monete/Monete/main.c
@@ -2,8 +2,9 @@
 extern int CombinaMonete(int b, const int* m, size_t m_size);
 int main(void) {
    
-    int m[] = {1, 2, 50, 10, 5, 20};
+    int m[] = {2, 50, 10, 5, 20};
     int sol = 0;
-    sol = CombinaMonete(4, m, 6);
+    sol = CombinaMonete(36, m, 5);
+    printf("\n%d soluzioni\n", sol);
     return 0;
 }
--- LAB/Esame di Laboratorio 21/Monete/monete.c
+++ LAB/Esame di Laboratorio 21/Monete/monete.c
@@ -1,67 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-bool ConfrontVector(int *v1, int *v2, size_t size){
-    if(!v1 || !v2){
-        return true;
-    }
-    
-    for (size_t i = 0; i != size; ++i){
-        if(v1[i] != v2[i]){
-            return true;
-        }
-    }
-    return false;
-}
-int *CreaVector(const int *m,size_t m_size, int *vccur){
-    int *vector = calloc(m_size, sizeof(int));
-    for (size_t i = 0; i != m_size; ++i){
-        for (size_t j = 0; j != m_size; ++j){
-            if(vccur[j] == m[i]){
-                ++vector[i];
-            }
-        }
-        
-    }
-    return vector;
-}
 void Stampa(size_t m_size, int *v){
     for (size_t i = 0; i != m_size; ++i){
         printf("%d ", v[i]);
     }
     printf("\n");
 }
-void CombinaMoneteRec(int b, const int* m, size_t m_size, int *n_sol, int count, int *vccur, int sum, int **v){
+void CombinaMoneteRec(int b, const int* m, size_t m_size, int *n_sol, int sum, int *v, size_t start_index){
     if(sum == b){
-        if(ConfrontVector(*v, CreaVector(m, m_size, vccur), m_size)){
-            *v = CreaVector(m, m_size, vccur);
-            Stampa(m_size, *v);
-            ++(*n_sol);
+        Stampa(m_size, v);
+        ++(*n_sol);
+        return;
+    }
+    if(sum > b){
+        return;
+    }
+    for (size_t i = start_index; i < m_size ; ++i) {
+            ++v[i];
+            CombinaMoneteRec(b, m, m_size, n_sol, sum + m[i], v, i);
+            --v[i];
+            
         }
-        return;
-    }
-    if(sum > b || count == (int)m_size){
-        return;
-    }
-
-    
-    for (size_t i = 0; i != m_size; ++i) {
-        vccur[count] = m[i];
-        CombinaMoneteRec(b, m, m_size,n_sol, count +1 , vccur, sum+vccur[count], v);
-        vccur[count] = 0;
-    }
-    
     
 }
 int CombinaMonete(int b, const int* m, size_t m_size){
-    
-    
-    int *vccur = calloc(m_size, sizeof(int));
     int n_sol = 0;
-    int *v = NULL;
-    CombinaMoneteRec(b, m, m_size, &n_sol, 0, vccur,0, &v);
-    
-    free(vccur);
+    int *v = calloc(m_size, sizeof(int));
+    CombinaMoneteRec(b, m, m_size, &n_sol, 0, v,0);
     free(v);
     return n_sol;
 }
