d120371b1ec4a750cabbe71cdbe7d21ebd56e49c
MarinCervinschi <marincervinschi3@gmail.com>
2023-07-03 12:56:38 +0200

    esame 22

---
+++ LAB/Esame di Laboratorio 22/Parentesi/Parentesi/main.c
@@ -0,0 +1,7 @@
+extern int Parentesi(int n);
+int main(void) {
+   
+    int res = 0;
+    res = Parentesi(3);
+    return 0;
+}
---
+++ LAB/Esame di Laboratorio 22/Parentesi/parentesi.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+bool verifica(int n, char *vccur){
+    int count = 0;
+    bool *k = calloc(n*2, sizeof(bool));
+    for (int i = 0; i != n*2-1; ++i) {
+        if(vccur[0] == ')' || vccur[n*2-1] == '('){
+            return false;
+        }
+        if(vccur[i] == '('){
+            for (int j = i+1; j != n*2; ++j){
+                if(vccur[j] == ')' && !k[j]){
+                    k[j] = true;
+                    ++count;
+                    break;
+                }
+            }
+        }
+    }
+    if(count != n){
+        free(k);
+        return false;
+    }else {
+        free(k);
+        return true;
+    }
+}
+void Stampa(int n, char *vccur){
+    for (int i = 0; i != n*2; ++i) {
+        printf("%c", vccur[i]);
+    }
+    printf("\n");
+}
+static void ParentesiRec(int n, int count, int *n_sol, char *vccur){
+    
+    if(count == n*2){
+        if(verifica(n, vccur)){
+            Stampa(n,vccur);
+            ++(*n_sol);
+        }
+        
+        return;
+    }
+    
+    vccur[count] = '(';
+    ParentesiRec(n, count + 1, n_sol, vccur);
+    vccur[count] = ')';
+    ParentesiRec(n, count + 1, n_sol, vccur);
+}
+
+int Parentesi(int n){
+    if(n <= 0){
+        return -1;
+    }
+    
+    char *vccur = calloc(n*2, sizeof(char));
+    int n_sol = 0;
+    ParentesiRec(n, 0, &n_sol, vccur);
+    
+    free(vccur);
+    return n_sol;
+        
+}
