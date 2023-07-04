9ac7912a2037622d87ae240cd32e8774e7018d26
MarinCervinschi <marincervinschi3@gmail.com>
2023-07-04 10:29:58 +0200

    exam 21

--- LAB/Esame di Laboratorio 21/Differenza/differenza.c
+++ LAB/Esame di Laboratorio 21/Differenza/differenza.c
@@ -3,11 +3,55 @@
 Item *Copia(const Item *i1){
     Item *tmp = NULL;
     while (i1) {
-        tmp = ListInsertHead(&i1->value, tmp);
+        tmp = ListInsertBack(tmp, &i1->value);
         i1 = i1->next;
     }
     return tmp;
 }
+
+ElemType Num(int s,int m, int *p){
+    s -= *p;
+    *p = 0;
+    if(s < m){
+        *p = 1;
+        s += 10;
+    }
+    return (s - m)+'0';
+}
 Item *Differenza(const Item *i1, const Item *i2){
-    if
+    if(!i1 && !i2){
+        return NULL;
+    }
+    if(!i1){
+        return Copia(i2);
+    }
+    if(!i2){
+        return Copia(i1);
+    }
+    Item *diff = NULL;
+    int p = 0;
+    while (1) {
+        if(!i1 && !i2){
+            break;
+        }
+        
+        ElemType s =0, m = 0;
+        if(i1){
+            s = atoi(&i1->value);
+        }
+        if(i2){
+            m = atoi(&i2->value);
+        }
+        
+        const ElemType iter = Num(s, m, &p);
+        diff = ListInsertBack(diff, &iter);
+        
+        if(i1){
+            i1 = i1->next;
+        }
+        if(i2){
+            i2= i2->next;
+        }
+    }
+    return diff;
 }
