b13936daa2f0245b4a926b039b9f66c5c0aeb3ea
MarinCervinschi <marincervinschi3@gmail.com>
2023-07-09 14:59:13 +0200

    algoritmi di ordinamento

--- LAB/Make Dir/Make Dir/main.c
+++ LAB/Make Dir/Make Dir/main.c
@@ -5,13 +5,16 @@
 #include <stdlib.h>
 int main(void) {
     char *directoryPath = "/Users/marin/Documents/STRUTTURE DATI E ALGORITMI/ALGORITMI DI ORDINAMENTO/";
-    char *directoryName = calloc(25, sizeof(char));
+    char *directoryName = calloc(50, sizeof(char));
     FILE *f = fopen("data.txt", "r");
-    char *path = calloc(100, sizeof(char));
+    char *path = calloc(200, sizeof(char));
     while (fscanf(f, "%[^\n]", directoryName) == 1) {
         directoryName = realloc(directoryName, strlen(directoryName) * sizeof(char));
-        snprintf(path, 100, "%s%s", directoryPath, directoryName);
+        snprintf(path, 200, "%s%s", directoryPath, directoryName);
+        //strcat(path, "/Complessità.txt");
         path = realloc(path, strlen(path)* sizeof(char));
+        FILE *k = fopen(path, "w");
+        fclose(k);
         mkdir(path, 0777);
         fseek(f, 1, SEEK_CUR);
     }
