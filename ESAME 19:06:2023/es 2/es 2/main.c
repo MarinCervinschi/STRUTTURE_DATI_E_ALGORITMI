bee1baf6c7a2cc03414068168c9f6c6a84911daa
MarinCervinschi <marincervinschi3@gmail.com>
2023-07-21 01:02:44 +0200

    subarrays

--- ESAME 19:06:2023/es 2/es 2/main.c
+++ ESAME 19:06:2023/es 2/es 2/main.c
@@ -1,11 +1,87 @@
 #include <stdio.h>
 
 extern int OptimalSubarray(const int *v, size_t n);
+void PrintStdoutVector(int *v, int v_size){
+    if (!v || v_size == 0) {
+        return;
+    }
+    printf("{");
+    for (int i = 0; i != v_size-1; ++i) {
+        printf("%d, ",v[i]);
+    }
+    printf("%d}\n", v[v_size-1]);
+}
 int main(void) {
-    
-    int v[] = { 1, 2, 1, -100, 5, 6};
-    
-    int ret = 0;
-    ret = OptimalSubarray(v, 6);
+    {
+        int v[] = {  };
+        size_t size = sizeof(v) / sizeof(int);
+        PrintStdoutVector(v, (int)size);
+        
+        printf("%d è la somma massima dei sottovettori ottimi di elementi contigui\n", OptimalSubarray(v, size));
+        printf("\n");
+    }
+    {
+        int v[] = {  1, 2, 3, 4, 5, 6};
+        size_t size = sizeof(v) / sizeof(int);
+        PrintStdoutVector(v, (int)size);
+        
+        printf("%d è la somma massima dei sottovettori ottimi di elementi contigui\n", OptimalSubarray(v, size));
+        printf("\n");
+    }
+    {
+        int v[] = { 1, 2, 1, -100, 5, 6};
+        size_t size = sizeof(v) / sizeof(int);
+        PrintStdoutVector(v, (int)size);
+        
+        printf("%d è la somma massima dei sottovettori ottimi di elementi contigui\n", OptimalSubarray(v, size));
+        printf("\n");
+    }
+    {
+        int v[] = { -100, 5, 6, -5, 1, 2, 1 };
+        size_t size = sizeof(v) / sizeof(int);
+        PrintStdoutVector(v, (int)size);
+        
+        printf("%d è la somma massima dei sottovettori ottimi di elementi contigui\n", OptimalSubarray(v, size));
+        printf("\n");
+    }
+    {
+        int v[] = { -100, 5, 6, -3, 1, 2, 1 };
+        size_t size = sizeof(v) / sizeof(int);
+        PrintStdoutVector(v, (int)size);
+        
+        printf("%d è la somma massima dei sottovettori ottimi di elementi contigui\n", OptimalSubarray(v, size));
+        printf("\n");
+    }
+    {
+        int v[] = { -100, 5, 6, -3, 1, 2, 1, 3};
+        size_t size = sizeof(v) / sizeof(int);
+        PrintStdoutVector(v, (int)size);
+        
+        printf("%d è la somma massima dei sottovettori ottimi di elementi contigui\n", OptimalSubarray(v, size));
+        printf("\n");
+    }
+    {
+        int v[] = { -1, -2, -3, -4, -5, -6};
+        size_t size = sizeof(v) / sizeof(int);
+        PrintStdoutVector(v, (int)size);
+        
+        printf("%d è la somma massima dei sottovettori ottimi di elementi contigui\n", OptimalSubarray(v, size));
+        printf("\n");
+    }
+    {
+        int v[] = { 1, 1, 1, -5, 1, 1, 1, -5, 1, 1, 1};
+        size_t size = sizeof(v) / sizeof(int);
+        PrintStdoutVector(v, (int)size);
+        
+        printf("%d è la somma massima dei sottovettori ottimi di elementi contigui\n", OptimalSubarray(v, size));
+        printf("\n");
+    }
+    {
+        int v[] = {  1, 1, 1, -5, -1, -1, -1, -5, -1, -1, -1};
+        size_t size = sizeof(v) / sizeof(int);
+        PrintStdoutVector(v, (int)size);
+        
+        printf("%d è la somma massima dei sottovettori ottimi di elementi contigui\n", OptimalSubarray(v, size));
+    }
     return 0;
 }
--- ESAME 19:06:2023/es 2/subarrays.c
+++ ESAME 19:06:2023/es 2/subarrays.c
@@ -1,42 +1,69 @@
-#include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-bool verifica(size_t n, bool *vccur){
-    for (size_t i = 0; i != n-1; ++i) {
-        if(vccur[i] && !vccur[i+1]){
-            return false;
+
+// Funzione per verificare se gli elementi di un sottovettore sono contigui
+bool verifica(size_t n, bool *vccur) {
+    int num_sott_vet = 0;
+    int start = -1;
+
+    // Scorre il vettore di booleani per contare quanti elementi (true) ci sono nel sottovettore e memorizza
+    // l'indice del primo elemento (true) trovato
+    for (size_t i = 0; i < n; ++i) {
+        if (vccur[i]) {
+            if (start == -1) {
+                start = (int)i;
+            }
+            ++num_sott_vet;
         }
     }
-    return true;
+
+    // Verifica se gli elementi tra l'indice di start e l'indice di start + num_sott_vet sono contigui
+    for (int i = 0; i < num_sott_vet; ++i) {
+        if (!vccur[start]) {
+            return false; // Gli elementi non sono contigui
+        }
+        ++start;
+    }
+
+    return true; // Gli elementi sono contigui
 }
-void OptimalSubarrayRec(const int *v, size_t n, int count, int *somma_max, int somma, bool *vccur){
-    if(count == (int)n){
-        if(somma > *somma_max && verifica(n, vccur)){
-            *somma_max = somma;
-        }
-        return;
-    }
-    if(somma > *somma_max && verifica(n, vccur)){
+
+// Funzione ricorsiva per trovare la somma massima di un sottovettore contiguo
+void OptimalSubarrayRec(const int *v, size_t n, int count, int *somma_max, int somma, bool *vccur) {
+    // Se la somma corrente del sottovettore è maggiore di somma_max e il sottovettore è contiguo, aggiorna somma_max
+    if (somma > *somma_max && verifica(n, vccur)) {
         *somma_max = somma;
     }
 
-    
+    // Caso base della ricorsione: tutte le combinazioni possibili sono state esplorate
+    if (count == (int)n) {
+        return;
+    }
+
+    // Includi l'elemento corrente nel sottovettore e passa alla successiva chiamata ricorsiva
     vccur[count] = true;
-    OptimalSubarrayRec(v, n, count + 1, somma_max, somma+v[count], vccur);
+    OptimalSubarrayRec(v, n, count + 1, somma_max, somma + v[count], vccur);
+
+    // Escludi l'elemento corrente dal sottovettore e passa alla successiva chiamata ricorsiva
     vccur[count] = false;
     OptimalSubarrayRec(v, n, count + 1, somma_max, somma, vccur);
-    
-    return ;
 }
-int OptimalSubarray(const int *v, size_t n){
+
+// Funzione principale
+int OptimalSubarray(const int *v, size_t n) {
+    // S e il vettore è vuoto, la somma massima del sottovettore contiguo è 0
     if (n == 0) {
         return 0;
     }
+
     int somma_max = 0;
-    
+
+    // Vettore di booleani per rappresentare il sottovettore corrente
     bool *vccur = calloc(n, sizeof(bool));
+
+    // Chiama la funzione ricorsiva per calcolare la somma massima di un sottovettore contiguo
     OptimalSubarrayRec(v, n, 0, &somma_max, 0, vccur);
-    
+
     free(vccur);
     return somma_max;
 }
