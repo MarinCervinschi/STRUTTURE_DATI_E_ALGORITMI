7c8e9a9f949941e7b8bf978602a468be7f97c58b
MarinCervinschi <marincervinschi3@gmail.com>
2023-07-25 16:54:32 +0200

    sort

--- LAB/Esame di Laboratorio 10/Insertion Sort/insertionsort.c
+++ LAB/Esame di Laboratorio 10/Insertion Sort/insertionsort.c
@@ -3,11 +3,11 @@ void InsertionSort(int *v, size_t v_size){
     if (!v || v_size < 2) {
         return;
     }
-    size_t i = 0, j = 0, key = 0;
+    int j = 0, key = 0;
     
-    for (i = 1; i < v_size; i++) {
+    for (size_t i = 1; i < v_size; i++) {
         key = v[i];
-        j = i - 1;
+        j = (int)i - 1;
         
         // Sposta gli elementi maggiori di key dietro di una posizione
         while (j >= 0 && v[j] > key) {
@@ -15,7 +15,7 @@ void InsertionSort(int *v, size_t v_size){
             j--;
         }
         // Inserisci la chiave nella sua posizione corretta
-        v[j + 1] = (int)key;
+        v[j + 1] = key;
     }
 }
 
