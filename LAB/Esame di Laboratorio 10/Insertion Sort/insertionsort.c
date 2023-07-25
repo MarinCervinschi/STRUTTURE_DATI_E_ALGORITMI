f3d6902732ec4ebc36af898438d2b0e400dc6a16
MarinCervinschi <marincervinschi3@gmail.com>
2023-07-25 16:49:18 +0200

    sort

--- LAB/Esame di Laboratorio 10/Insertion Sort/insertionsort.c
+++ LAB/Esame di Laboratorio 10/Insertion Sort/insertionsort.c
@@ -1,18 +1,21 @@
 #include <stdlib.h> // for size_t
-void ElemSwap(int *a, int *b){
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
 void InsertionSort(int *v, size_t v_size){
     if (!v || v_size < 2) {
         return;
     }
-    for (size_t i = 1; i < v_size; ++i) {
-        for (size_t j = 0; j < i; ++j) {
-            if(v[j] > v[i]){
-                ElemSwap(&v[j], &v[i]);
-            }
+    size_t i = 0, j = 0, key = 0;
+    
+    for (i = 1; i < v_size; i++) {
+        key = v[i];
+        j = i - 1;
+        
+        // Sposta gli elementi maggiori di key dietro di una posizione
+        while (j >= 0 && v[j] > key) {
+            v[j + 1] = v[j];
+            j--;
         }
+        // Inserisci la chiave nella sua posizione corretta
+        v[j + 1] = (int)key;
     }
 }
+
