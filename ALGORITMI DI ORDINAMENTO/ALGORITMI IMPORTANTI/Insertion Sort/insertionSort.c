d32dc5bd2630af0b67d766aa66361ab4f361c263
MarinCervinschi <marincervinschi3@gmail.com>
2023-11-13 19:01:30 +0100

    insertion Sort

--- ALGORITMI DI ORDINAMENTO/ALGORITMI IMPORTANTI/Insertion Sort/insertionSort.c
+++ ALGORITMI DI ORDINAMENTO/ALGORITMI IMPORTANTI/Insertion Sort/insertionSort.c
@@ -1,16 +1,11 @@
 void insertionSort(int arr[], int size) {
-    int i, j, key;
+    int i, j;
     
     for (i = 1; i < size; i++) {
-        key = arr[i];
-        j = i - 1;
-        
-        // Sposta gli elementi maggiori di key dietro di una posizione
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
+        j = i;
+        while ((j > 0) && (arr[j] < arr[j - 1])) {
+            swap(&s[j], &s[j - 1]);
             j--;
         }
-        // Inserisci la chiave nella sua posizione corretta
-            arr[j + 1] = key;
     }
 }
